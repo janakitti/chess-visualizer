83f1064cf99fcc1a28844261024b818025077881
Janakitti <janakittiratana@gmail.com>
2020-12-19 21:36:56 -0500

    Use opacity for shading

--- src/Square.js
+++ src/Square.js
@@ -3,17 +3,18 @@ import React from "react"
 function Square(props) {
     function generateShade(index) {
         const val = props.piece.val;
-        console.log(val)
-        const shadeFactor = 16;
-        let squareColor = ((index % 2 === 0 && (Math.floor(index / 8) % 2) === 0) ||
-            (index % 2 === 1 && ((Math.floor(index / 8) % 2) === 1))) ? 4671303 : 2367777
+        const shadeFactor = 2;
+        let squareColor = {r:50, g:50, b:50, a:1}
         if(val < 0) {
-            console.log("hello")
-            squareColor += val * shadeFactor * -65536
+            squareColor = {r:200, g:50, b:50, a:(val * -shadeFactor)/10};
         } else if(val > 0) {
-            squareColor += val * shadeFactor * 256
+            squareColor = {r:50, g:200, b:50, a:(val * shadeFactor)/10};
+        } else {
+            squareColor = ((index % 2 === 0 && (Math.floor(index / 8) % 2) === 0) ||
+                (index % 2 === 1 && ((Math.floor(index / 8) % 2) === 1))) ? {r:202, g:212, b:219, a:1} : {r:171, g:180, b:186, a:1}
         }
-        return "#" + squareColor.toString(16)
+        console.log(squareColor.a)
+        return "rgba(" + squareColor.r + ", " + squareColor.g + ", " + squareColor.b + ", " + squareColor.a + ")"
     }
     return (
         <button className="square"
