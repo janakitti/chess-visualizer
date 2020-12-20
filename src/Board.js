c8c898960695322afdfb81fa96293655d4388c56
Janakitti <janakittiratana@gmail.com>
2020-12-19 19:46:55 -0500

    Complete Knight and Rook

--- src/Board.js
+++ src/Board.js
@@ -18,37 +18,66 @@ class Board extends React.Component {
 
     analyze(pos, piece, grid) {
         let vals = []
+        const valMult = piece.player === "w" ? 1 : -1
         if (piece.type === "n") {
-            vals = [
-                {x:-1, y:2, v:1},
-                {x:1, y:2, v:1},
-                {x:-1, y:-2, v:1},
-                {x:1, y:-2, v:1},
-                {x:-2, y:1, v:1},
-                {x:2, y:1, v:1},
-                {x:-2, y:-1, v:1},
-                {x:2, y:-1, v:1}
-            ]
+            vals = [[
+                {x:-1, y:2, v:1*valMult},
+                {x:1, y:2, v:1*valMult},
+                {x:-1, y:-2, v:1*valMult},
+                {x:1, y:-2, v:1*valMult},
+                {x:-2, y:1, v:1*valMult},
+                {x:2, y:1, v:1*valMult},
+                {x:-2, y:-1, v:1*valMult},
+                {x:2, y:-1, v:1*valMult}
+            ]]
         } else if (piece.type === "r") {
-            for(let i=-7; i<0; i++) {
-                vals.push({x:i, y:0, v:1})
+            let path = []
+            for(let i=-1; i>=-7; i--) {
+                path.push({x:i, y:0, v:1*valMult})
             }
+            vals.push(path);
+            path = [];
             for(let i=1; i<=7; i++) {
-                vals.push({x:i, y:0, v:1})
+                path.push({x:i, y:0, v:1*valMult})
             }
-            for(let i=-7; i<0; i++) {
-                vals.push({x:0, y:i, v:1})
+            vals.push(path);
+            path = [];
+            for(let i=-1; i>=-7; i--) {
+                path.push({x:0, y:i, v:1*valMult})
             }
+            vals.push(path);
+            path = [];
             for(let i=1; i<=7; i++) {
-                vals.push({x:0, y:i, v:1})
+                path.push({x:0, y:i, v:1*valMult})
             }
+            vals.push(path);
         }
-        
-        vals = vals.filter(val => 
-            ((pos % 8) + val.x >= 0 && (pos % 8) + val.x <= 7) &&
-            (Math.floor(pos / 8) + val.y >= 0 && Math.floor(pos / 8) + val.y <= 7)
-        )
-        vals.forEach(v => {
+
+        console.log("VALS: ", vals);
+        vals = vals.map(path => {
+            let withinBoard = path.filter(val =>
+                ((pos % 8) + val.x >= 0 && (pos % 8) + val.x <= 7) &&
+                (Math.floor(pos / 8) + val.y >= 0 && Math.floor(pos / 8) + val.y <= 7)
+            )
+            console.log("WB: ", withinBoard);
+            let legalMoves = withinBoard;
+            for(let i = 0; i < withinBoard.length; i++) {
+                const curPos = pos + withinBoard[i].x + 8*withinBoard[i].y
+                if(grid[curPos].player === grid[pos].player) {
+                    legalMoves = withinBoard.slice(0, i);
+                    break;
+                } else if (grid[curPos].player !== "" && grid[curPos].player !== grid[pos].player) {
+                    legalMoves = withinBoard.slice(0, i + 1);
+                    break;
+                }
+            }
+            console.log("LM: ", legalMoves);
+            return legalMoves
+        })
+
+        const flattenVals = vals.flat();
+        console.log("FLATTEN: ", flattenVals);
+        flattenVals.forEach(v => {
             grid[pos + v.x + 8*v.y].val = grid[pos + v.x + 8*v.y].val + (v.v)
         })
 
