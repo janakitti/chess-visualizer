d3e8a68e62a75f0695c73852ed7f609a3833c670
Janakitti <janakittiratana@gmail.com>
2021-10-09 10:19:01 -0400

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Board from './Board';
+import Board from "./Board";
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-
-    }
-  }
-
   render() {
     return (
       <div className="app-body">
-      <div className="header">
+        <div className="header">
           <div>
-              <h1 className="title">Chess Visualizer</h1>
-              <h3 className="subtitle">Piece Path Visualizer</h3>
+            <h1 className="title">Chess Visualizer</h1>
+            <h3 className="subtitle">Piece Path Visualizer</h3>
           </div>
-      </div>
-
+        </div>
         <div className="board-container">
           <Board />
         </div>
       </div>
-    )
+    );
   }
 }
 
